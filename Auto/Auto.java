7bb347e0d7b9d63f6f111485cbf5127cac64316c
Mauron <simego@hotmail.it>
2015-10-14 20:47:21 +0200

    Update Auto.java

--- Auto/Auto.java
+++ Auto/Auto.java
@@ -1,15 +1,15 @@
 /**
-Questa classe genera oggetti di tipo Auto caratterizzati da una marca, un nome,
-una targa, una capacità serbatoio e da un valore di Km per litro.
+	Questa classe genera oggetti di tipo Auto caratterizzati da una marca, un nome,
+	una targa, una capacità serbatoio e da un valore di Km per litro.
 */
 public class Auto {
 	/**
-	Costruttore classe Auto
-	@param marca Il produttore dell'autoveicolo.
-	@param nome Il nome dell'autoveicolo.
-	@param targa La targa con cui l'autoveicolo è stato immatricolato.
-	@param serbatoio La capienza del serbatoio dell'autoveicolo.
-	@param kmperlitro Il numero di kilometri percorribile dall'autoveicolo con un litro di carburante.
+		Costruttore classe Auto
+		@param marca Il produttore dell'autoveicolo.
+		@param nome Il nome dell'autoveicolo.
+		@param targa La targa con cui l'autoveicolo è stato immatricolato.
+		@param serbatoio La capienza del serbatoio dell'autoveicolo.
+		@param kmperlitro Il numero di kilometri percorribile dall'autoveicolo con un litro di carburante.
 	*/
 	public Auto(String marca, String nome, String targa, int serbatoio, int kmperlitro) {
 		mark = marca;
@@ -21,7 +21,7 @@ public class Auto {
 	
 	// metodi
 	/**
-	Ottieni dati auto
+		Ottieni dati auto
 	*/
 	public void getData() {
 		System.out.println(mark);
@@ -31,14 +31,14 @@ public class Auto {
 		System.out.println(kmperliter);
 	}
 	/**
-	Ottieni autonomia dell'auto
-	@return getAutonomy() l'autonomia del modello selezionato
+		Ottieni autonomia dell'auto
+		@return getAutonomy() l'autonomia del modello selezionato
 	*/
 	public double getAutonomy() {
 		return fuel * kmperliter;
 	}
 	
-	//variabili d'istanza
+	// variabili d'istanza
 	private String mark;
 	private String name;
 	private String plate;
