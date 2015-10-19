b3b979c5b8aaf6407d911d3173a74276799e3206
Mauron <simego@hotmail.it>
2015-10-19 08:36:57 +0200

    Add Bank class

---
+++ Bank/Bank.java
@@ -0,0 +1,27 @@
+/**
+ *  A bank is a collection of manageable bank accounts: they
+ *  can be created, their balance can be updated, and sums of
+ *  money can be transfered between accounts.
+ */
+public class Bank
+{
+	/**
+	 *  Constructs a bank without accounts.
+	 */
+	public Bank()
+	{
+		// corpo costruttore
+	}
+	
+	public void addAccount(initialBalance, customerName)
+	{
+		// corpo del metodo modificatore
+	}
+	
+	public void transfer(fromAccount, toAccount, amount)
+	{
+		// corpo del metodo modificatore
+	}
+	
+	// variabili d'istanza
+}
